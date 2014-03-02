9d47e6456f7dc4bc8c31529b5ad8f9e345e2f075
Ewout Fernhout <chocolade@extrapuur.nl>
2014-03-03 00:27:39 +0100

    include open sans in package

--- lib/dompdf/lib/fonts/dompdf_font_family_cache.php
+++ lib/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -78,4 +78,11 @@
     'normal' => DOMPDF_FONT_DIR . '5ffc7e496d2efff1121d16976e8578be',
     'bold' => DOMPDF_FONT_DIR . 'fa16cc67cfbb3d1319f7702b1bb90c84',
   ),
+  'open sans' => 
+  array (
+    'normal' => DOMPDF_FONT_DIR . 'a32da215612cdbd93b93eb74bcab3b40',
+    'bold' => DOMPDF_FONT_DIR . 'ec66c4851b9656cec2800868fb6ca4a3',
+    'italic' => DOMPDF_FONT_DIR . 'e1ed1c78d201fe6255daee12e8820a6d',
+    'bold_italic' => DOMPDF_FONT_DIR . 'c1583238aa915ad7e56d544a3a5f8f4e',
+  ),
 ) ?>
