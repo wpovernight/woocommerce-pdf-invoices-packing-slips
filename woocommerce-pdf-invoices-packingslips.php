89aacbe9954a93325d4e8fb7a2b6d8270d9a2333
Ewout Fernhout <chocolade@extrapuur.nl>
2014-10-23 20:10:33 +0200

    order notes function

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -299,7 +299,48 @@ if ( !class_exists( 'WooCommerce_PDF_Invoices' ) ) {
 				echo $field_label . nl2br ($custom_field);
 			}
 		}
-	
+
+		/**
+		 * Return/Show order notes
+		 */		
+		public function get_order_notes( $filter = 'customer' ) {
+			$args = array(
+				'post_id' 	=> $this->export->order->id,
+				'approve' 	=> 'approve',
+				'type' 		=> 'order_note'
+			);
+
+			remove_filter( 'comments_clauses', array( 'WC_Comments', 'exclude_order_comments' ), 10, 1 );
+
+			$notes = get_comments( $args );
+
+			add_filter( 'comments_clauses', array( 'WC_Comments', 'exclude_order_comments' ), 10, 1 );
+
+			if ( $notes ) {
+				foreach( $notes as $key => $note ) {
+					if ( $filter == 'customer' && !get_comment_meta( $note->comment_ID, 'is_customer_note', true ) ) {
+						unset($notes[$key]);
+					}
+					if ( $filter == 'private' && get_comment_meta( $note->comment_ID, 'is_customer_note', true ) ) {
+						unset($notes[$key]);
+					}					
+				}
+				return $notes;
+			}
+		}
+		public function order_notes( $filter = 'customer' ) {
+			$notes = $this->get_order_notes( $filter );
+			if ( $notes ) {
+				foreach( $notes as $note ) {
+					?>
+					<div class="note_content">
+						<?php echo wpautop( wptexturize( wp_kses_post( $note->comment_content ) ) ); ?>
+					</div>
+					<?php
+				}
+			}
+		}
+
 		/**
 		 * Return/Show the current date
 		 */
