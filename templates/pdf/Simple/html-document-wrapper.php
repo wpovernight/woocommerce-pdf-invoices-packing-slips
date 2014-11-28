3d186d94b6eaa6f708e2cbe08ebfb124fabeb02d
Ewout Fernhout <chocolade@extrapuur.nl>
2014-11-28 09:15:45 +0100

    custom styles action

--- templates/pdf/Simple/html-document-wrapper.php
+++ templates/pdf/Simple/html-document-wrapper.php
@@ -4,7 +4,8 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title><?php echo ($wpo_wcpdf->export->template_type == 'invoice')?__( 'Invoice', 'wpo_wcpdf' ):__( 'Packing Slip', 'wpo_wcpdf' ) ?></title>
-	<style><?php $wpo_wcpdf->template_styles(); ?></style>
+	<style type="text/css"><?php $wpo_wcpdf->template_styles(); ?></style>
+	<style type="text/css"><?php do_action( 'wpo_wcpdf_custom_styles', $wpo_wcpdf->export->template_type ); ?></style>
 </head>
 <body class="<?php echo $wpo_wcpdf->export->template_type; ?>">
 <?php echo $wpo_wcpdf->export->output_body; ?>
