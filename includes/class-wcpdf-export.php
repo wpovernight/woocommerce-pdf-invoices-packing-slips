cf98ca365367a9ec94a57be1a10661346b15a61a
Ewout Fernhout <chocolade@extrapuur.nl>
2015-04-03 17:19:49 +0200

    explicitly exclude customer_new_account email

--- includes/class-wcpdf-export.php
+++ includes/class-wcpdf-export.php
@@ -472,7 +472,7 @@ if ( ! class_exists( 'WooCommerce_PDF_Invoices_Export' ) ) {
 			}
 
 			// do not process low stock notifications, user emails etc!
-			if ( in_array( $status, array( 'no_stock', 'low_stock', 'backorder' ) ) || get_post_type( $order->id ) != 'shop_order' ) {
+			if ( in_array( $status, array( 'no_stock', 'low_stock', 'backorder', 'customer_new_account' ) ) || get_post_type( $order->id ) != 'shop_order' ) {
 				return $attachments; 
 			}
 
