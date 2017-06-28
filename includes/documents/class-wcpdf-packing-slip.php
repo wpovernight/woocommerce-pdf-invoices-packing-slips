c7117b6cf00464a336022599080ad16bdccc1f83
Ewout Fernhout <info@drwoo.nl>
2017-06-28 13:56:57 +0200

    use correct text-domain for document title method
    closes #30

--- includes/documents/class-wcpdf-packing-slip.php
+++ includes/documents/class-wcpdf-packing-slip.php
@@ -38,7 +38,7 @@ class Packing_Slip extends Order_Document_Methods {
 
 	public function get_title() {
 		// override/not using $this->title to allow for language switching!
-		return apply_filters( "wpo_wcpdf_{$this->slug}_title", __( 'Packing Slip', 'wpo_wcpdf' ) );
+		return apply_filters( "wpo_wcpdf_{$this->slug}_title", __( 'Packing Slip', 'woocommerce-pdf-invoices-packing-slips' ) );
 	}
 
 	public function get_settings() {
