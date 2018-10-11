78c48b86d18840019bbe78ab73ed877aff756284
Ewout Fernhout <info@drwoo.nl>
2018-10-11 09:07:30 +0200

    Exit templates if accessed directly

--- templates/Simple/packing-slip.php
+++ templates/Simple/packing-slip.php
@@ -1,3 +1,4 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 <?php do_action( 'wpo_wcpdf_before_document', $this->type, $this->order ); ?>
 
 <table class="head container">
