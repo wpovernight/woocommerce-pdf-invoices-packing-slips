fd83b31305194f3481f21d2a871f0816ccde560e
Ewout Fernhout <info@drwoo.nl>
2018-12-04 14:14:34 +0100

    2.2.5

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce PDF Invoices & Packing Slips
  * Plugin URI: http://www.wpovernight.com
  * Description: Create, print & email PDF invoices & packing slips for WooCommerce orders.
- * Version: 2.2.4
+ * Version: 2.2.5
  * Author: Ewout Fernhout
  * Author URI: http://www.wpovernight.com
  * License: GPLv2 or later
@@ -21,7 +21,7 @@ if ( !class_exists( 'WPO_WCPDF' ) ) :
 
 class WPO_WCPDF {
 
-	public $version = '2.2.4';
+	public $version = '2.2.5';
 	public $plugin_basename;
 	public $legacy_mode;
 
