7342876be166efb16ad681e5328a422683f625c3
Terminator Barbapapa <Terminator-Barbapapa@users.noreply.github.com>
2019-09-11 18:01:28 +0200

    Add extra check for user meta regarding action buttons

--- includes/views/setup-wizard/show-action-buttons.php
+++ includes/views/setup-wizard/show-action-buttons.php
@@ -6,8 +6,14 @@
 </div>
 <div class="wpo-setup-input">
 	<?php
+	$actions = true;
 	$user_id = get_current_user_id();
 	$hidden = get_user_meta( $user_id, 'manageedit-shop_ordercolumnshidden', true );
+	if ( empty( $hidden ) )
+		$hidden = array( 'shipping_address', 'billing_address', 'wc_actions' );
+		update_user_option( $user_id, 'manageedit-shop_ordercolumnshidden', $hidden, true );
+	if ( in_array( 'wc_actions', $hidden ) )
+		$actions = false
 	?>
-	<input type="checkbox" <?php echo !in_array( 'wc_actions', $hidden ) ? 'checked' : ''; ?> name="wc_show_action_buttons" value="1"><span class="checkbox"><?php _e( 'Show action buttons', 'woocommerce-pdf-invoices-packing-slips' ); ?></span><br>
+	<input type="checkbox" <?php echo $actions !== false ? 'checked' : ''; ?> name="wc_show_action_buttons" value="1"><span class="checkbox"><?php _e( 'Show action buttons', 'woocommerce-pdf-invoices-packing-slips' ); ?></span><br>
 </div>
