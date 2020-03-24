37891cd8f54cfe42eb17159e8889e93179dc0ac4
Terminator Barbapapa <Terminator-Barbapapa@users.noreply.github.com>
2020-03-24 15:04:11 +0100

    Add wpo_wcpdf_pdf_data filter to bulk document

--- includes/documents/class-wcpdf-bulk-document.php
+++ includes/documents/class-wcpdf-bulk-document.php
@@ -60,7 +60,7 @@ class Bulk_Document {
 			'font_subsetting'	=> $this->wrapper_document->get_setting( 'font_subsetting', false ),
 		);
 		$pdf_maker = wcpdf_get_pdf_maker( $html, $pdf_settings );
-		$pdf = $pdf_maker->output();
+		$pdf = apply_filters( 'wpo_wcpdf_pdf_data', $pdf_maker->output(), $this, $is_bulk = true );
 		
 		do_action( 'wpo_wcpdf_after_pdf', $this->get_type(), $this );
 
