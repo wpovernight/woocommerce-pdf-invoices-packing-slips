d3aed9be39750dcbe903eb019da0d3148e935e38
Terminator Barbapapa <Terminator-Barbapapa@users.noreply.github.com>
2020-03-29 15:00:23 +0200

    Remove 'is_bulk' parameter in wpo_wcpdf_pdf_data filter from order and bulk document

--- includes/documents/class-wcpdf-bulk-document.php
+++ includes/documents/class-wcpdf-bulk-document.php
@@ -61,7 +61,7 @@ class Bulk_Document {
 			'font_subsetting'	=> $this->wrapper_document->get_setting( 'font_subsetting', false ),
 		);
 		$pdf_maker = wcpdf_get_pdf_maker( $html, $pdf_settings );
-		$pdf = apply_filters( 'wpo_wcpdf_pdf_data', $pdf_maker->output(), $this, $is_bulk = true );
+		$pdf = apply_filters( 'wpo_wcpdf_pdf_data', $pdf_maker->output(), $this );
 		
 		do_action( 'wpo_wcpdf_after_pdf', $this->get_type(), $this );
 
