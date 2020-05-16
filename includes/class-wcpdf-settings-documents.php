9300cbb1d012ea0891b46514f2098923cbe8abcc
Ewout Fernhout <info@drwoo.nl>
2020-05-16 10:33:49 +0200

    fix settings section for empty document titles

--- includes/class-wcpdf-settings-documents.php
+++ includes/class-wcpdf-settings-documents.php
@@ -31,7 +31,11 @@ class Settings_Documents {
 				<ul>
 					<?php
 					foreach ($documents as $document) {
-						printf('<li><a href="%s" class="%s">%s</a></li>', add_query_arg( 'section', $document->get_type() ), $document->get_type() == $section ? 'active' : '', $document->get_title() );
+						$title = strip_tags($document->get_title());
+						if (empty(trim($title))) {
+							$title = '['.__( 'untitled', 'woocommerce-pdf-invoices-packing-slips' ).']';
+						}
+						printf('<li><a href="%s" class="%s">%s</a></li>', add_query_arg( 'section', $document->get_type() ), $document->get_type() == $section ? 'active' : '', $title );
 					}
 					?>
 				</ul>
@@ -45,12 +49,21 @@ class Settings_Documents {
 			?>
 			<p><?php _e('All available documents are listed below. Click on a document to configure it.', 'woocommerce-pdf-invoices-packing-slips' ); ?></p>
 			<table class="wcpdf_documents_settings_list">
-				<?php $c = false; foreach ($documents as $document): ?>
-				<tr class="<?php echo (($c = !$c)?"odd":"even"); ?>">
-					<td class="title"><a href="<?php echo add_query_arg( 'section', $document->get_type() ); ?>"><?php echo $document->get_title(); ?></a></td>
-					<td class="settings-icon"><a href="<?php echo add_query_arg( 'section', $document->get_type() ); ?>"><span class="dashicons dashicons-admin-settings"></span></a></td>
-				</tr>
-				<?php endforeach ?>
+				<?php
+				$c = false;
+				foreach ($documents as $document) {
+					$title = strip_tags($document->get_title());
+					if (empty(trim($title))) {
+						$title = __( 'untitled', 'woocommerce-pdf-invoices-packing-slips' );
+					}
+					?>
+					<tr class="<?php echo (($c = !$c)?"odd":"even"); ?>">
+						<td class="title"><a href="<?php echo add_query_arg( 'section', $document->get_type() ); ?>"><?php echo $title; ?></a></td>
+						<td class="settings-icon"><a href="<?php echo add_query_arg( 'section', $document->get_type() ); ?>"><span class="dashicons dashicons-admin-settings"></span></a></td>
+					</tr>
+					<?php
+				}
+				?>
 			</table>
 			<?php
 		}
