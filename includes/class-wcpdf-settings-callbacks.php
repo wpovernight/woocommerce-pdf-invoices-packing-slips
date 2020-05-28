6f9fe5d137ac5544a0ef87a15a7e86681e4785f0
Alexandre Miguel <alexmigf@gmail.com>
2020-05-28 12:36:51 +0100

    Bug fix
    With only !isset doesn't apply the default value if current is empty

--- includes/class-wcpdf-settings-callbacks.php
+++ includes/class-wcpdf-settings-callbacks.php
@@ -80,10 +80,6 @@ class Settings_Callbacks {
 			$type = 'text';
 		}
 
-		if (empty($current) && isset($default)) {
-			$current = $default;
-		}
-
 		printf( '<input type="%1$s" id="%2$s" name="%3$s" value="%4$s" size="%5$s" placeholder="%6$s" %7$s/>', $type, $id, $setting_name, esc_attr( $current ), $size, $placeholder, !empty($disabled) ? 'disabled="disabled"' : '' );
 	
 		// output description.
@@ -533,7 +529,7 @@ class Settings_Callbacks {
 		}
 
 		// falback to default or empty if no value in option
-		if ( !isset($args['current']) ) {
+		if ( !isset($args['current']) || empty($args['current']) ) {
 			$args['current'] = isset( $args['default'] ) ? $args['default'] : '';
 		}
 
