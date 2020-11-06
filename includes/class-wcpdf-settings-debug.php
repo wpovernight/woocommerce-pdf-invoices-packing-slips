fd3684b776c9227de3dbf87c9b07d2cf279f3b37
Alexandre Faustino <alexmigf@gmail.com>
2020-11-06 22:59:52 +0000

    Better name the two buttons the same 'Protect PDF files directory'

--- includes/class-wcpdf-settings-debug.php
+++ includes/class-wcpdf-settings-debug.php
@@ -42,7 +42,7 @@ class Settings_Debug {
 				<form method="post">
 					<?php wp_nonce_field( 'wpo_wcpdf_debug_tools_action', 'security' ); ?>
 					<input type="hidden" name="wpo_wcpdf_debug_tools_action" value="generate_random_string">
-					<input type="submit" name="submit" id="submit" class="button" value="<?php _e( 'Generate random string', 'woocommerce-pdf-invoices-packing-slips' ); ?>">
+					<input type="submit" name="submit" id="submit" class="button" value="<?php _e( 'Protect PDF files directory', 'woocommerce-pdf-invoices-packing-slips' ); ?>">
 					<?php
 					if ( !empty($_POST) && isset($_POST['wpo_wcpdf_debug_tools_action']) && $_POST['wpo_wcpdf_debug_tools_action'] == 'generate_random_string' ) {
 						// check permissions
