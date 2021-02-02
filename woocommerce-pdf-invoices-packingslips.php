2c9d4a74e342c08ee14ffcb166bf625d7ccc924d
Alexandre Faustino <alexmigf@gmail.com>
2021-02-02 16:09:58 +0000

    Fixes last commit

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -211,10 +211,14 @@ class WPO_WCPDF {
 			return;
 		}
 
+		if ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
+			add_action( 'admin_notices', array ( $this, 'required_php_version' ) );
+			return;
+		}
+
 		if ( has_filter( 'wpo_wcpdf_pdf_maker' ) === false && version_compare( PHP_VERSION, '7.1', '<' ) ) {
 			add_filter( 'wpo_wcpdf_document_is_allowed', '__return_false', 99999 );
 			add_action( 'admin_notices', array ( $this, 'required_php_version' ) );
-			return;
 		}
 
 		// all systems ready - GO!
