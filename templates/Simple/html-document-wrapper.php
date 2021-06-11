68d435e6b121470c4001054d8fff098b098bb418
Alexandre Faustino <alexmigf@gmail.com>
2021-06-11 12:36:12 +0100

    New filter 'wpo_wcpdf_body_class' to allow override the template body class.
    #184

--- templates/Simple/html-document-wrapper.php
+++ templates/Simple/html-document-wrapper.php
@@ -7,7 +7,7 @@
 	<style type="text/css"><?php $this->template_styles(); ?></style>
 	<style type="text/css"><?php do_action( 'wpo_wcpdf_custom_styles', $this->get_type(), $this ); ?></style>
 </head>
-<body class="<?php echo $this->get_type(); ?>">
+<body class="<?php echo apply_filters( 'wpo_wcpdf_body_class', $this->get_type(), $this ); ?>">
 <?php echo $content; ?>
 </body>
 </html>
