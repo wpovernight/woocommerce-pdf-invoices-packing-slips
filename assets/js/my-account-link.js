7fc5b55737924fc5bde77c96d25d22d54d275ac2
Darren Peyou <74280840+dpeyou@users.noreply.github.com>
2021-08-04 14:33:38 -0600

    Fix: Prevent JS console notices on My Account page (#204)

--- assets/js/my-account-link.js
+++ assets/js/my-account-link.js
@@ -1,8 +1,11 @@
 jQuery( function( $ ) {
 	$('a').each(function(e){
-		// for lack of specific classes on the my account action buttons we check the url
-		if( $(this).attr('href').indexOf('generate_wpo_wcpdf') != -1 ){
-			$(this).attr('target', '_blank');
-		};
+		//check if href attribute exists
+		if ( $(this).attr('href') ) {
+			// for lack of specific classes on the my account action buttons we check the url
+			if( $(this).attr('href').indexOf('generate_wpo_wcpdf') != -1 ){
+				$(this).attr('target', '_blank');
+			};
+		}
 	});
 });
