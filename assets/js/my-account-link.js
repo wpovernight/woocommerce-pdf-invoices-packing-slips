31e74291da5f790405e9801cae568479f943ecdd
Darren Peyou <dpeyou@pop-os.localdomain>
2021-08-11 11:45:06 -0600

    adding space at start of comment

--- assets/js/my-account-link.js
+++ assets/js/my-account-link.js
@@ -1,6 +1,6 @@
 jQuery( function( $ ) {
 	$('a').each(function(e){
-		//check if href attribute exists
+		// check if href attribute exists
 		if ( $(this).attr('href') ) {
 			// for lack of specific classes on the my account action buttons we check the url
 			if( $(this).attr('href').indexOf('generate_wpo_wcpdf') != -1 ){
