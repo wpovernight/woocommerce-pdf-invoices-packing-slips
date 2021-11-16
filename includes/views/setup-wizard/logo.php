dfba9c85d29793667e31a019a712b56cfd62c372
Ewout Fernhout <info@drwoo.nl>
2021-11-16 21:01:44 +0100

    use settings callback for logo step in wizard

--- includes/views/setup-wizard/logo.php
+++ includes/views/setup-wizard/logo.php
@@ -4,18 +4,15 @@
 	<p><?php esc_html_e( 'Set the header image that will display on your invoice.' , 'woocommerce-pdf-invoices-packing-slips' ); ?></p>
 </div>
 <div class="wpo-setup-input">
+	<script><?php echo "var wpo_wcpdf_admin = " . wp_json_encode( array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) ) . ';'; ?></script>
 	<?php
-	$current_settings = get_option( 'wpo_wcpdf_settings_general', array() );
-	$logo_id = !empty($current_settings['header_logo']) ? $current_settings['header_logo'] : '';
-
-	if ( !empty($logo_id) && $logo = wp_get_attachment_image_src( $logo_id, 'full', false ) ) {
-		$logo_src = $logo[0];
-	} else {
-		// empty image
-		$logo_src = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
-	}
+	WPO_WCPDF()->settings->callbacks->media_upload( array(
+		'option_name'          => 'wpo_wcpdf_settings_general',
+		'setting_name'         => 'wcpdf_settings[wpo_wcpdf_settings_general][header_logo]',
+		'id'                   => 'header_logo',
+		'uploader_title'       => __( 'Select or upload your invoice header/logo', 'woocommerce-pdf-invoices-packing-slips' ),
+		'uploader_button_text' => __( 'Set image', 'woocommerce-pdf-invoices-packing-slips' ),
+		'remove_button_text'   => __( 'Remove image', 'woocommerce-pdf-invoices-packing-slips' ),
+	) );
 	?>
-	<img src="<?php echo $logo_src; ?>" width="100%" height="20px" alt="" id="img-header_logo"/>
-	<input id="header_logo" name="wcpdf_settings[wpo_wcpdf_settings_general][header_logo]" type="hidden" value="<?php echo $logo_id; ?>" />
-	<span class="button wpo_upload_image_button header_logo" data-uploader_title="<?php esc_html_e( 'Select or upload your invoice header/logo', 'woocommerce-pdf-invoices-packing-slips' ); ?>" data-uploader_button_text="<?php esc_html_e( 'Set image', 'woocommerce-pdf-invoices-packing-slips' ); ?>" data-remove_button_text="<?php esc_html_e( 'Remove image', 'woocommerce-pdf-invoices-packing-slips' ); ?>" data-input_id="header_logo"><?php esc_html_e( 'Set image', 'woocommerce-pdf-invoices-packing-slips' ); ?></span>
 </div>
