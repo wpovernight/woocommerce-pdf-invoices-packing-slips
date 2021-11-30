6d045feb5455c5bb1a8e1b502e0b883aa84d328e
Ewout Fernhout <info@drwoo.nl>
2021-11-30 13:27:32 +0100

    Shop name escaping

--- includes/views/setup-wizard/shop-name.php
+++ includes/views/setup-wizard/shop-name.php
@@ -1,7 +1,7 @@
 <?php defined( 'ABSPATH' ) or exit; ?>
 <div class="wpo-step-description">
-	<h2><?php _e( 'Enter your shop name', 'woocommerce-pdf-invoices-packing-slips' ); ?></h2>
-	<p><?php _e( 'Lets quickly setup your invoice. Please enter the name and address of your shop in the fields on the right.', 'woocommerce-pdf-invoices-packing-slips' ); ?></p>
+	<h2><?php esc_html_e( 'Enter your shop name', 'woocommerce-pdf-invoices-packing-slips' ); ?></h2>
+	<p><?php esc_html_e( 'Lets quickly setup your invoice. Please enter the name and address of your shop in the fields on the right.', 'woocommerce-pdf-invoices-packing-slips' ); ?></p>
 </div>
 <div class="wpo-setup-input">
 	<?php
