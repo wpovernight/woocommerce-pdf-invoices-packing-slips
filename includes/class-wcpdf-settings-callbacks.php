043f5340373774c1146dce91627e9d3e6e6c0c6e
Alexandre Faustino <alexmigf@gmail.com>
2021-12-23 15:06:53 +0000

    Prevents unwanted queries when settings API are called outside the usual scopes
    #253

--- includes/class-wcpdf-settings-callbacks.php
+++ includes/class-wcpdf-settings-callbacks.php
@@ -215,6 +215,10 @@ class Settings_Callbacks {
 			printf( '<select id="%1$s" name="%2$s">', esc_attr( $id ), esc_attr( $setting_name ) );
 		}
 
+		if ( ! empty( $options_callback ) ) {
+			$options = ! empty( $options_callback_args ) ? call_user_func_array( $options_callback, $options_callback_args ) : call_user_func( $options_callback );
+		}
+
 		foreach ( $options as $key => $label ) {
 			if ( ! empty( $multiple ) && is_array( $current ) ) {
 				$selected = in_array( $key, $current ) ? ' selected="selected"' : '';
@@ -266,6 +270,10 @@ class Settings_Callbacks {
 
 	public function radio_button( $args ) {
 		extract( $this->normalize_settings_args( $args ) );
+
+		if ( ! empty( $options_callback ) ) {
+			$options = ! empty( $options_callback_args ) ? call_user_func_array( $options_callback, $options_callback_args ) : call_user_func( $options_callback );
+		}
 	
 		foreach ( $options as $key => $label ) {
 			printf( '<input type="radio" class="radio" id="%1$s[%3$s]" name="%2$s" value="%3$s"%4$s />', esc_attr( $id ), esc_attr( $setting_name ), esc_attr( $key ), checked( $current, $key, false ) );
@@ -287,6 +295,10 @@ class Settings_Callbacks {
 	 */
 	public function multiple_text_input( $args ) {
 		extract( $this->normalize_settings_args( $args ) );
+
+		if ( ! empty( $fields_callback ) ) {
+			$fields = ! empty( $fields_callback_args ) ? call_user_func_array( $fields_callback, $fields_callback_args ) : call_user_func( $fields_callback );
+		}
 
 		if ( ! empty( $header ) ) {
 			echo wp_kses_post( "<p><strong>{$header}</strong>:</p>" );
@@ -330,6 +342,10 @@ class Settings_Callbacks {
 	 */
 	public function multiple_checkboxes( $args ) {
 		extract( $this->normalize_settings_args( $args ) );
+
+		if ( ! empty( $fields_callback ) ) {
+			$fields = ! empty( $fields_callback_args ) ? call_user_func_array( $fields_callback, $fields_callback_args ) : call_user_func( $fields_callback );
+		}
 
 		foreach ( $fields as $name => $label ) {
 			// output checkbox	
@@ -412,20 +428,31 @@ class Settings_Callbacks {
 	 */
 	public function next_number_edit( $args ) {
 		extract( $args ); // $store, $size, $description
-		// Sequential_Number_Store object
-		if( is_object( $store ) ) {
-			$next_number         = $store->get_next();
-			$store               = $store->store_name;
-		// legacy
-		} else {
-			$number_store_method = WPO_WCPDF()->settings->get_sequential_number_store_method(); 
-			$number_store        = new Sequential_Number_Store( $store, $number_store_method ); 
-			$next_number         = $number_store->get_next();
+
+		$store_name  = '';
+		$next_number = '';
+
+		if( ! empty( $store_callback ) ) {
+			// Sequential_Number_Store object
+			if ( $store_callback instanceof Sequential_Number_Store ) {
+				$store = $store_callback;
+			// array with document object + function name
+			} elseif ( is_array( $store_callback ) ) {
+				$store = ! empty( $store_callback_args ) ? call_user_func_array( $store_callback, $store_callback_args ) : call_user_func( $store_callback );
+			// legacy
+			} else {
+				$number_store_method = WPO_WCPDF()->settings->get_sequential_number_store_method(); 
+				$store               = new Sequential_Number_Store( $store_callback, $number_store_method ); 
+			}
+
+			$store_name  = $store->store_name;
+			$next_number = $store->get_next();
 		}
-		$nonce = wp_create_nonce( "wpo_wcpdf_next_{$store}" );
+
+		$nonce = wp_create_nonce( "wpo_wcpdf_next_{$store_name}" );
 		printf(
 			'<input id="next_%1$s" class="next-number-input" type="text" size="%2$s" value="%3$s" disabled="disabled" data-store="%1$s" data-nonce="%4$s"/> <span class="edit-next-number dashicons dashicons-edit"></span><span class="save-next-number button secondary" style="display:none;">%5$s</span>',
-			esc_attr( $store ),
+			esc_attr( $store_name ),
 			esc_attr( $size ),
 			esc_attr( $next_number ),
 			esc_attr( $nonce ),
