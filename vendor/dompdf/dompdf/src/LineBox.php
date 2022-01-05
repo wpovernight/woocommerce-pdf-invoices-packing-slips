d1de782126850c37e7ce8f5708a02c93c3a04565
Alexandre Faustino <alexmigf@gmail.com>
2022-01-05 09:25:29 +0000

    Revert "Revert "Applies patch from dompdf/dompdf/tree/whitespace""
    This reverts commit 0e892abd54a9a0844566f3218687cbae1f65217a.

--- vendor/dompdf/dompdf/src/LineBox.php
+++ vendor/dompdf/dompdf/src/LineBox.php
@@ -10,6 +10,7 @@ namespace Dompdf;
 use Dompdf\FrameDecorator\AbstractFrameDecorator;
 use Dompdf\FrameDecorator\Block;
 use Dompdf\FrameDecorator\Page;
+use Dompdf\FrameReflower\Text as TextFrameReflower;
 use Dompdf\Positioner\Inline as InlinePositioner;
 
 /**
@@ -304,6 +305,26 @@ class LineBox
         }
 
         $this->h = $h;
+    }
+
+	/**
+     * Trim trailing whitespace from the line.
+     */
+    public function trim_trailing_ws(): void
+    {
+        $lastIndex = count($this->_frames) - 1;
+
+        if ($lastIndex < 0) {
+            return;
+        }
+
+        $lastFrame = $this->_frames[$lastIndex];
+        $reflower = $lastFrame->get_reflower();
+
+        if ($reflower instanceof TextFrameReflower && !$lastFrame->is_pre()) {
+            $reflower->trim_trailing_ws();
+            $this->recalculate_width();
+        }
     }
 
     /**
