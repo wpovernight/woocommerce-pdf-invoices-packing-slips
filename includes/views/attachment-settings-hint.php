c284c92c9492d77e98b5c81898ecb16f4d756d7c
Alexandre Faustino <alexmigf@gmail.com>
2022-01-06 14:57:34 +0000

    Fixes the attachment settings hint notice
    #281

--- includes/views/attachment-settings-hint.php
+++ includes/views/attachment-settings-hint.php
@@ -9,16 +9,15 @@ $invoice_settings_url = add_query_arg( array(
 .wcpdf-attachment-settings-hint {
 	display: inline-block;
 	background: #fff;
-	border-left: 4px solid #cc99c2 !important;
-	-webkit-box-shadow: 0 1px 1px 0 rgba( 0, 0, 0, 0.1 );
-	box-shadow: 0 1px 1px 0 rgba( 0, 0, 0, 0.1 );
+	border-left: 4px solid #51266b !important;
+	-webkit-box-shadow: 0 0 35px -8px rgba(0, 0, 0, 0.12);
+	box-shadow: 0 0 35px -8px rgba(0, 0, 0, 0.12);
 	padding: 15px;
 	margin-top: 15px;
-	font-size: 120%;
 }
 </style>
 <div class="wcpdf-attachment-settings-hint">
 	<?php /* translators: <a> tags */ ?>
-	<?php printf( wp_kses_post( __( 'It looks like you haven\'t setup any email attachments yet, check the settings under <b>%1$sDocuments > Invoice%2$s</b>', 'woocommerce-pdf-invoices-packing-slips' ) ), '<a href="'.$invoice_settings_url.'">', '</a>' ); ?><br>
-	<?php printf( '<a href="%s" style="font">%s</a>', add_query_arg( 'wpo_wcpdf_hide_attachments_hint', 'true' ), esc_html__( 'Hide this message', 'woocommerce-pdf-invoices-packing-slips' ) ); ?>
+	<?php printf( wp_kses_post( __( 'It looks like you haven\'t setup any email attachments yet, check the settings under <b>%1$sDocuments > Invoice%2$s</b>', 'woocommerce-pdf-invoices-packing-slips' ) ), '<a href="'.$invoice_settings_url.'">', '</a>' ); ?>
+	<?php printf( '<a href="%s" style="display:block; margin-top:10px;">%s</a>', add_query_arg( 'wpo_wcpdf_hide_attachments_hint', 'true' ), esc_html__( 'Hide this message', 'woocommerce-pdf-invoices-packing-slips' ) ); ?>
 </div>
