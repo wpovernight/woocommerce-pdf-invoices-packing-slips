d01050a099b35a58332c2bb3acd9947a16029099
Ewout Fernhout <info@drwoo.nl>
2022-01-11 14:52:56 +0100

    PHP8.1: Suppress incompatible return type notice

--- includes/compatibility/class-wc-date-compatibility.php
+++ includes/compatibility/class-wc-date-compatibility.php
@@ -11,23 +11,24 @@ if ( ! class_exists( '\\WPO\\WC\\PDF_Invoices\\Compatibility\\WC_DateTime' ) ) :
 
 
 /**
- * WC Wrapper for PHP DateTime.
+ * WC Wrapper for PHP DateTime which adds support for gmt/utc offset when a
+ * timezone is absent
  *
- * @class    WC_DateTime
- * @since    3.0.0
- * @package  WooCommerce/Classes
- * @category Class
- * @author   WooThemes
+ * @since   3.0.0
+ * @package WooCommerce\Classes
  */
 class WC_DateTime extends \DateTime {
 
 	/**
-	 * UTC Offset if needed.
+	 * UTC Offset, if needed. Only used when a timezone is not set. When
+	 * timezones are used this will equal 0.
+	 *
 	 * @var integer
 	 */
 	protected $utc_offset = 0;
+
 	/**
-	 * Output an ISO 8601 date string in local timezone.
+	 * Output an ISO 8601 date string in local (WordPress) timezone.
 	 *
 	 * @since  3.0.0
 	 * @return string
@@ -35,39 +36,47 @@ class WC_DateTime extends \DateTime {
 	public function __toString() {
 		return $this->format( DATE_ATOM );
 	}
+
 	/**
-	 * Set UTC offset.
+	 * Set UTC offset - this is a fixed offset instead of a timezone.
+	 *
+	 * @param int $offset Offset.
 	 */
 	public function set_utc_offset( $offset ) {
 		$this->utc_offset = intval( $offset );
 	}
+
 	/**
-	 * getOffset.
+	 * Get UTC offset if set, or default to the DateTime object's offset.
 	 */
+	#[\ReturnTypeWillChange]
 	public function getOffset() {
-		if ( $this->utc_offset ) {
-			return $this->utc_offset;
-		} else {
-			return parent::getOffset();
-		}
+		return $this->utc_offset ? $this->utc_offset : parent::getOffset();
 	}
+
 	/**
 	 * Set timezone.
-	 * @param DateTimeZone $timezone
+	 *
+	 * @param DateTimeZone $timezone DateTimeZone instance.
+	 * @return DateTime
 	 */
+	#[\ReturnTypeWillChange]
 	public function setTimezone( $timezone ) {
 		$this->utc_offset = 0;
 		return parent::setTimezone( $timezone );
 	}
+
 	/**
-	 * Missing in PHP 5.2.
+	 * Missing in PHP 5.2 so just here so it can be supported consistently.
 	 *
 	 * @since  3.0.0
 	 * @return int
 	 */
+	#[\ReturnTypeWillChange]
 	public function getTimestamp() {
 		return method_exists( 'DateTime', 'getTimestamp' ) ? parent::getTimestamp() : $this->format( 'U' );
 	}
+
 	/**
 	 * Get the timestamp with the WordPress timezone offset added or subtracted.
 	 *
@@ -77,21 +86,23 @@ class WC_DateTime extends \DateTime {
 	public function getOffsetTimestamp() {
 		return $this->getTimestamp() + $this->getOffset();
 	}
+
 	/**
 	 * Format a date based on the offset timestamp.
 	 *
 	 * @since  3.0.0
-	 * @param  string $format
+	 * @param  string $format Date format.
 	 * @return string
 	 */
 	public function date( $format ) {
 		return gmdate( $format, $this->getOffsetTimestamp() );
 	}
+
 	/**
 	 * Return a localised date based on offset timestamp. Wrapper for date_i18n function.
 	 *
 	 * @since  3.0.0
-	 * @param  string $format
+	 * @param  string $format Date format.
 	 * @return string
 	 */
 	public function date_i18n( $format = 'Y-m-d' ) {
