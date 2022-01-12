f2dce32388f3344c93099e024eb281e9b9b8f080
Ewout Fernhout <info@drwoo.nl>
2022-01-12 15:09:01 +0100

    WC tested up to: 6.1

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -10,7 +10,7 @@
  * License URI: https://opensource.org/licenses/gpl-license.php
  * Text Domain: woocommerce-pdf-invoices-packing-slips
  * WC requires at least: 2.2.0
- * WC tested up to: 6.0.0
+ * WC tested up to: 6.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
