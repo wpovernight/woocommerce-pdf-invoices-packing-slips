ce7f5e73ba279b03f78513d1688eba63298174bc
Terminator Barbapapa <mux777@hotmail.com>
2022-05-23 10:48:42 +0200

    Revert including classes to the correct order

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -188,8 +188,8 @@ class WPO_WCPDF {
 		$this->settings = include_once( $this->plugin_path() . '/includes/class-wcpdf-settings.php' );
 		$this->documents = include_once( $this->plugin_path() . '/includes/class-wcpdf-documents.php' );
 		$this->main = include_once( $this->plugin_path() . '/includes/class-wcpdf-main.php' );
-		$this->admin = include_once( $this->plugin_path() . '/includes/class-wcpdf-admin.php' );
 		include_once( $this->plugin_path() . '/includes/class-wcpdf-assets.php' );
+		$this->admin = include_once( $this->plugin_path() . '/includes/class-wcpdf-admin.php' );
 		include_once( $this->plugin_path() . '/includes/class-wcpdf-frontend.php' );
 		include_once( $this->plugin_path() . '/includes/class-wcpdf-install.php' );
 		include_once( $this->plugin_path() . '/includes/class-wcpdf-font-synchronizer.php' );
