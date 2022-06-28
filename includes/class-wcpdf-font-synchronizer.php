de3f055345804a78a51fa043ba01333e0a00fd50
Alexandre Faustino <alexmigf@gmail.com>
2022-06-28 11:07:21 +0100

    Changes upon PR comments

--- includes/class-wcpdf-font-synchronizer.php
+++ includes/class-wcpdf-font-synchronizer.php
@@ -17,7 +17,7 @@ class Font_Synchronizer {
 	 *
 	 * @var string
 	 */
-	public $font_cache_filename = "installed-fonts.dist.json";
+	public $font_cache_filename = "installed-fonts.json";
 
 	/**
 	 * Vanilla instance of dompdf
@@ -48,7 +48,7 @@ class Font_Synchronizer {
 		$plugin_fonts = $this->get_plugin_fonts();
 		$dompdf_fonts = $this->get_dompdf_fonts();
 		if ( $merge_with_local ) {
-			$local_fonts  = $this->get_local_fonts( $destination );
+			$local_fonts = $this->get_local_fonts( $destination );
 		} else {
 			$local_fonts = array();
 		}
@@ -73,7 +73,7 @@ class Font_Synchronizer {
 		// normalize one last time
 		$local_fonts = $this->normalize_font_paths( $local_fonts );
 		// rebuild font cache file
-		$cacheData   = json_encode( $local_fonts );
+		$cacheData   = json_encode( $local_fonts, JSON_PRETTY_PRINT );
 		// write file with merged cache data
 		file_put_contents( $destination . $this->font_cache_filename, $cacheData );
 	}
@@ -138,12 +138,12 @@ class Font_Synchronizer {
 		$cache_file        = trailingslashit( $path ) . $this->font_cache_filename;
 		$legacy_cache_file = trailingslashit( $path ) . 'dompdf_font_family_cache.php'; // Dompdf <2.0
 
-		if ( is_readable( $legacy_cache_file ) ) {
-			$font_data = include $legacy_cache_file;
-			@unlink( $legacy_cache_file );
-		} elseif ( is_readable( $cache_file ) ) {
+		if ( is_readable( $cache_file ) ) {
 			$json_data = file_get_contents( $cache_file );
 			$font_data = json_decode( $json_data, true );
+		} elseif ( is_readable( $legacy_cache_file ) ) {
+			$font_data = include $legacy_cache_file;
+			@unlink( $legacy_cache_file );
 		} else {
 			$font_data = array();
 		}
