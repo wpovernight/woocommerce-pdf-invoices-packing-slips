da7b070c638d080020f726210f0ab972456dd209
Alexandre Faustino <alexmigf@gmail.com>
2022-10-24 10:12:47 +0100

    WPCS

--- includes/documents/abstract-wcpdf-order-document.php
+++ includes/documents/abstract-wcpdf-order-document.php
@@ -624,18 +624,20 @@ abstract class Order_Document {
 	 * Show logo html
 	 */
 	public function header_logo() {
-		if ($this->get_header_logo_id()) {
+		if ( $this->get_header_logo_id() ) {
 			$attachment_id = $this->get_header_logo_id();
-			$company = $this->get_shop_name();
-			if( $attachment_id ) {
-				$attachment = wp_get_attachment_image_src( $attachment_id, 'full', false );
+			$company       = $this->get_shop_name();
+
+			if ( $attachment_id ) {
+				$attachment      = wp_get_attachment_image_src( $attachment_id, 'full', false );
 				$attachment_path = get_attached_file( $attachment_id );
+
 				if ( empty( $attachment ) || empty( $attachment_path ) ) {
 					return;
 				}
 				
-				$attachment_src = $attachment[0];
-				$attachment_width = $attachment[1];
+				$attachment_src    = $attachment[0];
+				$attachment_width  = $attachment[1];
 				$attachment_height = $attachment[2];
 
 				if ( apply_filters( 'wpo_wcpdf_use_path', true ) && file_exists( $attachment_path ) ) {
@@ -646,7 +648,7 @@ abstract class Order_Document {
 					return;
 				}
 				
-				$img_element = sprintf('<img src="%1$s" alt="%2$s" />', esc_attr( $src ), esc_attr( $company ) );
+				$img_element = sprintf( '<img src="%1$s" alt="%2$s" />', esc_attr( $src ), esc_attr( $company ) );
 				
 				echo apply_filters( 'wpo_wcpdf_header_logo_img_element', $img_element, $attachment, $this );
 			}
