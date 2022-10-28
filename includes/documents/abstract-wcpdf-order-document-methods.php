e46814da0cdf141763eaf22ab12761c6ad599680
Alexandre Faustino <alexmigf@gmail.com>
2022-10-28 12:17:33 +0100

    Small tweaks

--- includes/documents/abstract-wcpdf-order-document-methods.php
+++ includes/documents/abstract-wcpdf-order-document-methods.php
@@ -142,9 +142,9 @@ abstract class Order_Document_Methods extends Order_Document {
 	 */
 	public function get_phone( $phone_type = 'billing' ) {
 		$phone = '';
-		if ( !empty( $order = $this->is_refund( $this->order ) ? $this->get_refund_parent( $this->order ) : $this->order ) ) {
+		if ( ! empty( $order = $this->is_refund( $this->order ) ? $this->get_refund_parent( $this->order ) : $this->order ) ) {
 			$getter = "get_{$phone_type}_phone";
-			$phone = is_callable( array( $order, $getter ) ) ? call_user_func( array( $order, $getter ) ) : '';
+			$phone  = is_callable( array( $order, $getter ) ) ? call_user_func( array( $order, $getter ) ) : '';
 		}
 		return $phone;
 	}
