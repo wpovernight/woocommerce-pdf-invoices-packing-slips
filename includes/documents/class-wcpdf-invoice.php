c854351329a08a829155fa862b179f4dcbab328d
Alexandre Faustino <alexmigf@gmail.com>
2023-03-10 15:50:56 +0000

    Update class-wcpdf-invoice.php

--- includes/documents/class-wcpdf-invoice.php
+++ includes/documents/class-wcpdf-invoice.php
@@ -434,7 +434,7 @@ class Invoice extends Order_Document_Methods {
 					),
 					'multiple'         => true,
 					'enhanced_select'  => true,
-					'description'      => __( 'If enabled, allows you to mark the document as printed, manually (in the order page) or automatically (based on the document creation context you have selected).', 'woocommerce-pdf-invoices-packing-slips' ),
+					'description'      => __( 'Allows you to mark the document as printed, manually (in the order page) or automatically (based on the document creation context you have selected).', 'woocommerce-pdf-invoices-packing-slips' ),
 				)
 			),
 			array(
