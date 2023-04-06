07f550e12806200ec1ff4fdf241df7f681959591
Alexandre Faustino <alexmigf@gmail.com>
2023-04-06 12:33:06 +0100

    Update class-wcpdf-main.php

--- includes/class-wcpdf-main.php
+++ includes/class-wcpdf-main.php
@@ -1322,7 +1322,7 @@ class Main {
 	public function document_can_be_manually_marked_printed( $document ) {
 		$can_be_manually_marked_printed = false;
 		
-		if ( empty( $document ) || property_exists( $document, 'is_bulk' ) ) {
+		if ( empty( $document ) || ( property_exists( $document, 'is_bulk' ) && $document->is_bulk ) ) {
 			return $can_be_manually_marked_printed;
 		}
 		
