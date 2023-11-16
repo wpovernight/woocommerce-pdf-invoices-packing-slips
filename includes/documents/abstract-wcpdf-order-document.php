bcd20adffedc6f7bd75ed5f06e7c06232beba265
Alexandre Faustino <alexmigf@gmail.com>
2023-11-16 13:13:55 +0000

    Revert

--- includes/documents/abstract-wcpdf-order-document.php
+++ includes/documents/abstract-wcpdf-order-document.php
@@ -495,15 +495,7 @@ abstract class Order_Document {
 		return $this->get_data( 'number', $document_type, $order, $context );
 	}
 	
-	public function get_formatted_number( $document_type = null ) {
-		if ( empty( $document_type ) && is_callable( array( $this, 'get_type' ) ) ) {
-			$document_type = $this->get-type();
-			
-			if ( empty( $document_type ) ) {
-				return '';
-			}
-		}
-		
+	public function get_formatted_number( $document_type ) {
 		$number = $this->get_number( $document_type );
 		
 		if ( $number ) {
@@ -522,14 +514,6 @@ abstract class Order_Document {
 	}
 	
 	public function get_formatted_date( $document_type ) {
-		if ( empty( $document_type ) && is_callable( array( $this, 'get_type' ) ) ) {
-			$document_type = $this->get-type();
-			
-			if ( empty( $document_type ) ) {
-				return '';
-			}
-		}
-		
 		$date = $this->get_date( $document_type );
 		
 		if ( $date ) {
