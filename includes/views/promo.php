a5b7018ff55dd198025c094d59e2edd380084944
Alexandre Faustino <alexmigf@gmail.com>
2023-12-06 09:27:24 +0000

    Tweaks

--- includes/views/promo.php
+++ includes/views/promo.php
@@ -28,7 +28,7 @@
 	<p>Elevate your customer experience and streamline your operations with one of our powerful extensions.</p>
 	<p>Use promocode <strong class="code">blackfriday30</strong> to get a <strong>30% discount</strong> on your upgrade!</p>
 	<?php if ( ( isset( $_GET['tab'] ) && $_GET['tab'] !== 'upgrade' ) || ! isset( $_GET['tab'] ) ) : ?>
-		<p class="upgrade-tab">Check out the <a href="<?php echo esc_url( admin_url( 'admin.php?page=wpo_wcpdf_options_page&tab=upgrade' ) ) ?>">Upgrade tab</a> for a feature overview of our PDF Invoices & Packing Slips extensions.</p>
+		<p class="upgrade-tab">Check out the <a href="<?php echo esc_url( admin_url( 'admin.php?page=wpo_wcpdf_options_page&tab=upgrade' ) ); ?>">Upgrade tab</a> for a feature overview of our PDF Invoices & Packing Slips extensions.</p>
 	<?php  endif; ?>
 	<p class="expiration">Offer ends on November 29.</p>
 	<?php printf( '<a href="%s" class="dismiss" style="display:inline-block; margin-top: 10px;">%s</a>', esc_url( wp_nonce_url( add_query_arg( 'wpo_wcpdf_hide_promo_ad', 'true' ), 'hide_promo_ad_nonce' ) ), esc_html__( 'Hide this message', 'woocommerce-pdf-invoices-packing-slips' ) ); ?>
