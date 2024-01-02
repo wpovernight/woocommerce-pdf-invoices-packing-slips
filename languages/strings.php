c391fb4d30c4eaaf911a1fc5164c8542836fdc18
Yordan Soares <contacto@yordansoar.es>
2024-01-02 14:52:53 +0100

    Declare new "Payment Date:" string
    Currently, we are using the "Payment Date:" string within the Receipt document, but it has not yet been declared.

--- languages/strings.php
+++ languages/strings.php
@@ -2,6 +2,7 @@
 exit;
 __( 'VAT', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Tax rate', 'woocommerce-pdf-invoices-packing-slips' );
+__( 'Payment Date:', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Payment method', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Shipping method', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Send email', 'woocommerce-pdf-invoices-packing-slips' );
