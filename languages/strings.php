35462f175d0c8d35cc29374ce24d39a1f04f4b3b
Yordan Soares <38109855+YordanSoares@users.noreply.github.com>
2024-01-04 09:33:22 +0100

    New: declare new "Payment Date:" string (II) (#687)

--- languages/strings.php
+++ languages/strings.php
@@ -3,6 +3,7 @@ exit;
 __( 'VAT', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Tax rate', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Payment date', 'woocommerce-pdf-invoices-packing-slips' );
+__( 'Payment Date:', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Payment method', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Shipping method', 'woocommerce-pdf-invoices-packing-slips' );
 __( 'Send email', 'woocommerce-pdf-invoices-packing-slips' );
