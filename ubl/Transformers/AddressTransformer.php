c36902ac88d62af913eb3e58dc2ed8ec21f28097
Alexandre Faustino <alexmigf@gmail.com>
2024-04-01 12:43:08 +0100

    Update AddressTransformer.php

--- ubl/Transformers/AddressTransformer.php
+++ ubl/Transformers/AddressTransformer.php
@@ -15,18 +15,18 @@ class AddressTransformer {
 	 */
 	public function transform( \WC_Abstract_Order $item, $billing_or_shipping ) {
 		$model             = new Address();
-		$model->address_1  = $item->{'get_'.$billing_or_shipping.'_address_1'}();
-		$model->address_2  = $item->{'get_'.$billing_or_shipping.'_address_2'}();
-		$model->first_name = $item->{'get_'.$billing_or_shipping.'_first_name'}();
-		$model->last_name  = $item->{'get_'.$billing_or_shipping.'_last_name'}();
-		$model->city       = $item->{'get_'.$billing_or_shipping.'_city'}();
+		$model->address_1  = WPO_WCPDF()->sanitizer->sanitize_html( $item->{'get_'.$billing_or_shipping.'_address_1'}() );
+		$model->address_2  = WPO_WCPDF()->sanitizer->sanitize_html( $item->{'get_'.$billing_or_shipping.'_address_2'}() );
+		$model->first_name = WPO_WCPDF()->sanitizer->sanitize_html( $item->{'get_'.$billing_or_shipping.'_first_name'}() );
+		$model->last_name  = WPO_WCPDF()->sanitizer->sanitize_html( $item->{'get_'.$billing_or_shipping.'_last_name'}() );
+		$model->city       = WPO_WCPDF()->sanitizer->sanitize_html( $item->{'get_'.$billing_or_shipping.'_city'}() );
 		$model->state      = $item->{'get_'.$billing_or_shipping.'_state'}();
 		$model->postcode   = $item->{'get_'.$billing_or_shipping.'_postcode'}();
 		$model->country    = $item->{'get_'.$billing_or_shipping.'_country'}();
 
 		if ( 'billing' === $billing_or_shipping ) {
-			$model->email = $item->get_billing_email();
-			$model->phone = $item->get_billing_phone();
+			$model->email = WPO_WCPDF()->sanitizer->sanitize_email( $item->get_billing_email() );
+			$model->phone = WPO_WCPDF()->sanitizer->sanitize_phone( $item->get_billing_phone() );
 		}
 
 		return $model;
