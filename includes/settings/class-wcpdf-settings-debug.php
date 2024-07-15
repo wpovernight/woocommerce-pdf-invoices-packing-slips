1f43d90a6ef06230d29cd71afb745018d88d9804
Alexandre Faustino <alexmigf@gmail.com>
2024-07-15 09:50:14 +0100

    Tweak: improves header logo load. New: advanced embed images setting (#826)

--- includes/settings/class-wcpdf-settings-debug.php
+++ includes/settings/class-wcpdf-settings-debug.php
@@ -823,6 +823,18 @@ class Settings_Debug {
 			),
 			array(
 				'type'     => 'setting',
+				'id'       => 'embed_images',
+				'title'    => __( 'Embed Images', 'woocommerce-pdf-invoices-packing-slips' ),
+				'callback' => 'checkbox',
+				'section'  => 'debug_settings',
+				'args'     => array(
+					'option_name' => $option_name,
+					'id'          => 'embed_images',
+					'description' => __( 'Embed images only if you are experiencing issues with them loading in your PDF. Please note that this option can significantly increase the file size.', 'woocommerce-pdf-invoices-packing-slips' ),
+				)
+			),
+			array(
+				'type'     => 'setting',
 				'id'       => 'log_to_order_notes',
 				'title'    => __( 'Log to order notes', 'woocommerce-pdf-invoices-packing-slips' ),
 				'callback' => 'checkbox',
