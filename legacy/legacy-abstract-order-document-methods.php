75e9772c836ad356a10c24dd86d7aad313dc3b6b
Alexandre Faustino <alexmigf@gmail.com>
2024-07-15 17:14:56 +0100

    Improvement

---
+++ legacy/legacy-abstract-order-document-methods.php
@@ -0,0 +1,18 @@
+<?php
+namespace WPO\WC\PDF_Invoices\Documents;
+
+use WPO\IPS\Documents\OrderDocumentMethods;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( '\\WPO\\WC\\PDF_Invoices\\Documents\\Order_Document_Methods' ) ) :
+
+abstract class Order_Document_Methods extends OrderDocumentMethods {
+
+	// This is a legacy class, do not use it directly!
+
+}
+
+endif; // class_exists
