23bfcc843dc7be9eb6564003fb1ff3b30204decc
Alexandre Faustino <alexmigf@gmail.com>
2024-07-16 15:51:47 +0100

    Update legacy-abstract-order-document-methods.php

--- legacy/legacy-abstract-order-document-methods.php
+++ legacy/legacy-abstract-order-document-methods.php
@@ -12,6 +12,7 @@ if ( ! class_exists( '\\WPO\\WC\\PDF_Invoices\\Documents\\Order_Document_Methods
 abstract class Order_Document_Methods extends OrderDocumentMethods {
 
 	// This is a legacy class, do not use it directly!
+	// Required by the Professional extension for backwards compatibility, version 2.15.10 and older
 
 }
 
