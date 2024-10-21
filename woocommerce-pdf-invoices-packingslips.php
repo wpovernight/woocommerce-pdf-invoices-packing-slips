928561a281fafd5631b444730543257b9dea4d73
Alexandre Faustino <alexmigf@gmail.com>
2024-10-21 09:49:13 +0100

    v3.9.0

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -4,7 +4,7 @@
  * Requires Plugins:     woocommerce
  * Plugin URI:           https://wpovernight.com/downloads/woocommerce-pdf-invoices-packing-slips-bundle/
  * Description:          Create, print & email PDF or UBL Invoices & PDF Packing Slips for WooCommerce orders.
- * Version:              3.9.0-beta-3
+ * Version:              3.9.0
  * Author:               WP Overnight
  * Author URI:           https://www.wpovernight.com
  * License:              GPLv2 or later
@@ -22,7 +22,7 @@ if ( ! class_exists( 'WPO_WCPDF' ) ) :
 
 class WPO_WCPDF {
 
-	public $version              = '3.9.0-beta-3';
+	public $version              = '3.9.0';
 	public $version_php          = '7.2';
 	public $version_woo          = '3.3';
 	public $version_wp           = '4.4';
@@ -454,7 +454,7 @@ class WPO_WCPDF {
 			}
 		}
 	}
-	
+
 	/**
 	 * PHP below 7.4 notice
 	 *
@@ -492,12 +492,12 @@ class WPO_WCPDF {
 			} else {
 				update_option( 'wpo_wcpdf_hide_php_below_7_4_drop_notice', true );
 			}
-			
+
 			wp_redirect( 'admin.php?page=wpo_wcpdf_options_page' );
 			exit;
 		}
 	}
-	
+
 	/**
 	 * UBL requires PHP 7.4
 	 *
@@ -523,7 +523,7 @@ class WPO_WCPDF {
 			} else {
 				update_option( 'wpo_wcpdf_hide_ubl_php_notice', true );
 			}
-			
+
 			wp_redirect( 'admin.php?page=wpo_wcpdf_options_page' );
 			exit;
 		}
