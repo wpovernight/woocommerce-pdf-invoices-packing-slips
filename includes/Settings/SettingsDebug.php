c3a7224a4f329744d7f8948fb6e2fedbbb7167e9
Mohamad Nateqi Rostami <mnateqi@wpovernight.com>
2024-12-17 12:10:00 +0330

    Improvements

--- includes/Settings/SettingsDebug.php
+++ includes/Settings/SettingsDebug.php
@@ -1012,7 +1012,12 @@ class SettingsDebug {
 			),
 			'WP Memory Limit' => array(
 				/* translators: <a> tags */
-				'required' => sprintf( __( 'Recommended: 128MB (more for plugins with heavy setups)<br/>See: %1$sIncreasing the WordPress Memory Limit%2$s', 'woocommerce-pdf-invoices-packing-slips' ), '<a href="https://docs.woocommerce.com/document/increasing-the-wordpress-memory-limit/" target="_blank">', '</a>' ),
+				'required' => __( 'Recommended: 128MB (more for plugins with heavy setups)', 'woocommerce-pdf-invoices-packing-slips' ) . '<br/>' . sprintf(
+						/* translators: 1: opening anchor tag, 2: closing anchor tag */
+						__( 'See: %1$sIncreasing the WordPress Memory Limit%2$s', 'woocommerce-pdf-invoices-packing-slips' ),
+						'<a href="https://docs.woocommerce.com/document/increasing-the-wordpress-memory-limit/" target="_blank">',
+						'</a>'
+					),
 				'value'    => sprintf( 'WordPress: %s, PHP: %s', WP_MEMORY_LIMIT, $php_mem_limit ),
 				'result'   => $memory_limit > 67108864,
 			),
