a4217d374c37e307f0d138479a2fdc1f837b5082
Mohamad Nateqi Rostami <mnateqi@wpovernight.com>
2025-01-02 14:01:26 +0330

    Improvements

--- wpo-ips-ubl-functions.php
+++ wpo-ips-ubl-functions.php
@@ -6,20 +6,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /*
 |--------------------------------------------------------------------------
-| UBL Document global getter functions
+| UBL Document global functions
 |--------------------------------------------------------------------------
 */
-
-use WPO\IPS\Documents\OrderDocument;
 
 /**
  * Sanitizes a string for use in UBL documents by stripping all HTML tags and decoding HTML entities to plain text.
  *
- * @param $string
+ * @param string $string
  *
  * @return string
  */
-function wpo_ips_ubl_sanitize_string( $string ): string {
+function wpo_ips_ubl_sanitize_string( string $string ): string {
 	$string = wp_strip_all_tags( $string );
 	return htmlspecialchars_decode( $string, ENT_QUOTES );
 }
