e347fe1432058468cf3b1618f76836af98666e55
Alexandre Faustino <alexmigf@gmail.com>
2025-03-13 11:00:51 +0000

    Tweak

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'wpovernight/woocommerce-pdf-invoices-packing-slips',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '54eb6b2323353cfeaa5a2af3b93ee4782b2eac13',
+        'reference' => '414c1ab91529a84a9a5e80fc0b250aa6ffc3a2ae',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -94,7 +94,7 @@
         'wpovernight/woocommerce-pdf-invoices-packing-slips' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '54eb6b2323353cfeaa5a2af3b93ee4782b2eac13',
+            'reference' => '414c1ab91529a84a9a5e80fc0b250aa6ffc3a2ae',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
