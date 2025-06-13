4effbad4dea75893f6b41fa990a786e230894996
Alexandre Faustino <alexmigf@gmail.com>
2025-06-13 19:55:59 +0100

    Update order-script.js

--- assets/js/order-script.js
+++ assets/js/order-script.js
@@ -242,7 +242,7 @@ jQuery( function( $ ) {
 		} );
 	}
 	
-	$( '.wcpdf-data-fields .number-specs-table input, .wcpdf-data-fields .number-table input' ).on( 'input change', function() {
+	$( document ).on( 'input change', '.wcpdf-data-fields .number-specs-table input, .wcpdf-data-fields .number-table input', function() {
 		var $table = $( this ).closest( '.wcpdf-data-fields' );
 		updatePreviewNumber( $table );
 	} );
