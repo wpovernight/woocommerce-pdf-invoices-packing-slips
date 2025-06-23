39ef671cb21b0cec46f957be92302ef508682bc9
Alexandre Faustino <alexmigf@gmail.com>
2025-06-23 08:15:47 +0100

    Update SettingsDebug.php

--- includes/Settings/SettingsDebug.php
+++ includes/Settings/SettingsDebug.php
@@ -877,7 +877,7 @@ class SettingsDebug {
 					'id'          => 'enable_document_data_editing',
 					'description' => __( 'Allow editing of document number and date on the order page.', 'woocommerce-pdf-invoices-packing-slips' ) . '<br>' .
 									sprintf(
-										/* translators: 1. Note, 2. Document types */
+										/* translators: %1$s: note, %2$s: document types */
 										__( '%1$s: Changing document data is prohibited in some countries. This setting is disabled by default to comply with legal requirements. Only the following document types are affected: %2$s.', 'woocommerce-pdf-invoices-packing-slips' ),
 										'<strong>' . __( 'Note', 'woocommerce-pdf-invoices-packing-slips' ) . '</strong>',
 										'<code>' . __( 'Invoice', 'woocommerce-pdf-invoices-packing-slips' ) . '</code>' . ( function_exists( 'WPO_WCPDF_Pro' ) ? ', <code>' . __( 'Credit Note',  ) . '</code>' : 'woocommerce-pdf-invoices-packing-slips' )
