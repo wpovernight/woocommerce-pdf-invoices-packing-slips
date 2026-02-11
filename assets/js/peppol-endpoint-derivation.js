d9e89479e311797b73e9851af82f419331b0ce38
Alexandre Faustino <alexmigf@gmail.com>
2026-02-11 14:56:43 +0000

    Wip

--- assets/js/peppol-endpoint-derivation.js
+++ assets/js/peppol-endpoint-derivation.js
@@ -9,8 +9,9 @@
 			: [];
 
 		const DEBUG                   = !!CONFIG.debug;
-		const VAT_SELECTOR             = CONFIG.vat_field_selector;
-		const AUTOFILL_ENDPOINT_ROUTE  = CONFIG.peppol_autofill_endpoint_route;
+		const VAT_SELECTOR            = CONFIG.vat_field_selector;
+		const AUTOFILL_ENDPOINT_ROUTE = CONFIG.peppol_autofill_endpoint_route;
+		const OVERRIDE_LINK_ENABLED   = !!CONFIG.override_link;
 
 		function log( ...args ) {
 			if ( DEBUG ) console.log( '[WPO IPS Peppol]', ...args );
@@ -100,6 +101,13 @@
 
 			let link = wrapper.querySelector( '.wpo-ips-override' );
 
+			// If disabled by setting, ensure it's not shown (and bail).
+			if ( ! OVERRIDE_LINK_ENABLED ) {
+				if ( link ) link.remove();
+				return;
+			}
+
+			// Only show link when locked.
 			if ( ! locked ) {
 				if ( link ) link.remove();
 				return;
