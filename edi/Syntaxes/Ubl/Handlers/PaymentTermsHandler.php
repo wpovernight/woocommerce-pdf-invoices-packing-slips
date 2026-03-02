9c849e115791be6a15e2174ca4d90dc32072758b
Mohamad Nateqi Rostami <mnateqi@wpovernight.com>
2026-03-02 11:39:48 +0300

    Move filter for due date days to `get_due_date_days()`

--- edi/Syntaxes/Ubl/Handlers/PaymentTermsHandler.php
+++ edi/Syntaxes/Ubl/Handlers/PaymentTermsHandler.php
@@ -18,13 +18,7 @@ class PaymentTermsHandler extends AbstractUblHandler {
 	 */
 	public function handle( array $data, array $options = array() ): array {
 		$payment_terms = array();
-		$due_date_days = apply_filters(
-			'wpo_ips_edi_ubl_payment_terms_due_date_days',
-			$this->get_due_date_days(),
-			$data,
-			$options,
-			$this
-		);
+		$due_date_days = $this->get_due_date_days();
 
 		if ( ! empty( $due_date_days ) ) {
 			$payment_terms = array(
