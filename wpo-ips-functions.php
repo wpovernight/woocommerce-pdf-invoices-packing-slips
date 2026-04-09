5aa8f175cbf589763886f7c0cc2687d4c65c63aa
Alexandre Faustino <alexmigf@gmail.com>
2026-04-09 15:06:26 +0100

    Update wpo-ips-functions.php

--- wpo-ips-functions.php
+++ wpo-ips-functions.php
@@ -2279,13 +2279,8 @@ function wpo_ips_is_current_page_checkout_page(): bool {
  */
 function wpo_ips_register_additional_checkout_field( array $options ): void {
 	if ( ! function_exists( 'woocommerce_register_additional_checkout_field' ) && defined( 'WC_PLUGIN_FILE' ) ) {
-		$file                 = dirname( WC_PLUGIN_FILE ) . '/src/Blocks/Domain/Services/functions.php';
-		$file_system_instance = WPO_WCPDF()->file_system ?? null;
-		$file_system_instance = $file_system_instance
-			? $file_system_instance
-			: \WPO\IPS\Compatibility\FileSystem::instance();
-		
-		if ( $file_system_instance->is_readable( $file ) ) {
+		$file = dirname( WC_PLUGIN_FILE ) . '/src/Blocks/Domain/Services/functions.php';
+		if ( WPO_WCPDF()->get_instance( 'file_system' )->is_readable( $file ) ) {
 			include_once $file;
 		}
 	}
