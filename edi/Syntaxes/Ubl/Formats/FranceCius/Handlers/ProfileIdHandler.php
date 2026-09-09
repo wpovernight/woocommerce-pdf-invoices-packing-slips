97d9124a5e9f6677fedd62fa79239f6bfbfbace3
Alexandre Faustino <alexmigf@gmail.com>
2026-09-09 11:01:46 +0100

    Wip

---
+++ edi/Syntaxes/Ubl/Formats/FranceCius/Handlers/ProfileIdHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WPO\IPS\EDI\Syntaxes\Ubl\Formats\FranceCius\Handlers;
+
+use WPO\IPS\EDI\Syntaxes\Ubl\Abstracts\AbstractUblHandler;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+class ProfileIdHandler extends AbstractUblHandler {
+
+	/**
+	 * Handle the data and return the formatted output.
+	 *
+	 * @param array $data    The data to be handled.
+	 * @param array $options Additional options for handling.
+	 * @return array
+	 */
+	public function handle( array $data, array $options = array() ): array {
+		$profile_id = array(
+			'name'  => 'cbc:ProfileID',
+			'value' => 'B1',
+		);
+
+		$data[] = apply_filters( 'wpo_ips_edi_ubl_profile_id', $profile_id, $data, $options, $this );
+
+		return $data;
+	}
+
+}
--- edi/Syntaxes/Ubl/Formats/FranceCius/Invoice.php
+++ edi/Syntaxes/Ubl/Formats/FranceCius/Invoice.php
@@ -13,4 +13,31 @@ class Invoice extends UblInvoice {
 	public string $slug = 'france-cius-ubl-invoice';
 	public string $name = 'France CIUS UBL Invoice';
 
+	/**
+	 * Get the format structure
+	 *
+	 * @return array
+	 */
+	public function get_structure(): array {
+		$structure = parent::get_structure();
+
+		$profile_id = array(
+			'profile_id' => array(
+				'enabled' => true,
+				'handler' => \WPO\IPS\EDI\Syntaxes\Ubl\Formats\FranceCius\Handlers\ProfileIdHandler::class,
+			),
+		);
+
+		$keys     = array_keys( $structure );
+		$position = array_search( 'customization_id', $keys, true );
+
+		if ( false === $position ) {
+			return $structure;
+		}
+
+		return array_slice( $structure, 0, $position + 1, true )
+			+ $profile_id
+			+ array_slice( $structure, $position + 1, null, true );
+	}
+
 }
